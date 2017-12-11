99037086f3805bf9d65eea659c54942b7c86653b
Gregory Higley <code@revolucent.net>
2017-12-11 16:28:45 -0500

    Guise

---
+++ Guise/Guise.swift
@@ -0,0 +1,20 @@
+//
+//  Guise.swift
+//  Guise
+//
+//  Created by Gregory Higley on 12/11/17.
+//  Copyright © 2017 Gregory Higley. All rights reserved.
+//
+
+import Foundation
+
+public struct Guise {
+    enum Name {
+        case `default`
+    }
+    
+    enum Container {
+        case `default`
+        case injections
+    }
+}
