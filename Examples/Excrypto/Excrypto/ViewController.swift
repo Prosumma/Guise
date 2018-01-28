c3cb35f7b9c74d842166245c37fbfc696ec70843
Gregory Higley <code@revolucent.net>
2018-01-28 17:28:49 -0500

    ExCrypto Example Project

---
+++ Examples/Excrypto/Excrypto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Excrypto
+//
+//  Created by Gregory Higley on 1/28/18.
+//  Copyright © 2018 Prosumma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
