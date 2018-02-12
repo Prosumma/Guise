7fc0fa9be3ff6b6b255a9b6f23fc3a1f4c68b0a2
Gregory Higley <code@revolucent.net>
2018-02-12 00:45:26 -0500

    Skeleton

---
+++ Examples/Skeleton/Skeleton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Skeleton
+//
+//  Created by Gregory Higley on 2/12/18.
+//  Copyright © 2018 Revolucent LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
