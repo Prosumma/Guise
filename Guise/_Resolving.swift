d382a6a5d8c6f2dc19fad15ef3a4f9ac7c28dce8
Gregory Higley <code@revolucent.net>
2018-02-12 00:29:08 -0500

    _Resolving

--- Guise/_Resolving.swift
+++ Guise/_Resolving.swift
@@ -14,5 +14,10 @@ public protocol _Resolving {
     @discardableResult static func register<ParameterType, HoldingType: Holder>(key: Key<HoldingType.Held>, metadata: Any, cached: Bool, resolution: @escaping Resolution<ParameterType, HoldingType>) -> Key<HoldingType.Held>
     @discardableResult static func unregister<Keys: Sequence>(keys: Keys) -> Int where Keys.Element: Keyed
     static func filter<K: Keyed>(_ filter: @escaping (K) -> Bool) -> [K: Registration]
+    // MARK: Injection
+    static var injectables: Set<String> { get }
+    @discardableResult static func register(injectable key: String, injection: @escaping Injection<Any>) -> String
+    @discardableResult static func resolve<Target>(into target: Target) -> Target
+    @discardableResult static func unregister<Keys: Sequence>(keys: Keys) -> Int where Keys.Element == String
 }
 
