808db17a9321e7ee3b4c4539bfb376c51154bbe8
Gregory Higley <greg@prosumma.com>
2020-02-25 13:30:39 -0500

    Scope & Key

---
+++ Guise/Key.swift
@@ -0,0 +1,28 @@
+//
+//  Key.swift
+//  Guise
+//
+//  Created by Gregory Higley on 2/25/20.
+//  Copyright © 2020 Gregory Higley. All rights reserved.
+//
+
+import Foundation
+
+public struct Key: Hashable {
+    public let type: String
+    public let scope: Scope
+
+    public init(type: String, scope: Scope) {
+        self.type = type
+        self.scope = scope
+    }
+
+    public init<Type>(type: Type.Type, scope: Scope) {
+        self.init(type: String(reflecting: type), scope: scope)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(type)
+        scope.hash(into: &hasher)
+    }
+}
---
+++ Guise/Scope.swift
@@ -0,0 +1,54 @@
+//
+//  Scope.swift
+//  Guise
+//
+//  Created by Gregory Higley on 2/25/20.
+//  Copyright © 2020 Gregory Higley. All rights reserved.
+//
+
+import Foundation
+
+public struct Scope: Equatable {
+    private class Parent {
+        let scope: Scope
+        init(_ scope: Scope) {
+            self.scope = scope
+        }
+    }
+
+    private let _parent: Parent?
+    public let identifier: AnyHashable
+
+    private init() {
+        _parent = nil
+        identifier = "/"
+    }
+
+    public init(parent: Scope, identifier: AnyHashable = UUID()) {
+        self._parent = Parent(parent)
+        self.identifier = identifier
+    }
+
+    public var parent: Scope? {
+        return _parent?.scope
+    }
+
+    /// `Scope` is not `Hashable` but participates in the hashing of `Key`.
+    internal func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+        parent?.hash(into: &hasher)
+    }
+
+    public static func ==(lhs: Scope, rhs: Scope) -> Bool {
+        return lhs.identifier == rhs.identifier && lhs.parent == rhs.parent
+    }
+
+    public static let root = Scope()
+}
+
+infix operator ~>: MultiplicationPrecedence
+
+public func ~><R: Hashable>(lhs: Scope, rhs: R) -> Scope {
+    return Scope(parent: lhs, identifier: rhs)
+}
+
