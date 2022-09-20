71343e1982e78e723ee6d4d923c9c1053c815acb
Gregory Higley <greg@prosumma.com>
2022-09-20 19:19:49 -0400

    Initial commit of redo

--- Package.swift
+++ Package.swift
@@ -1,35 +1,28 @@
-// swift-tools-version:5.0
-//
-//  Package.swift
-//  Guise
-//
-//  Created by Gregory Higley on 6/8/19.
-//  Copyright © 2019 Gregory Higley. All rights reserved.
-//
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Guise",
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Guise",
-            targets: ["Guise"])
+            targets: ["Guise"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Guise",
-            path: "Guise"),
+            dependencies: []),
         .testTarget(
             name: "GuiseTests",
-            dependencies: ["Guise"],
-            path: "GuiseTests")
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: ["Guise"]),
+    ]
 )
