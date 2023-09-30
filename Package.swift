ee34e16b89a4a7a47f3f9e82f34ff48c9761f49f
Gregory Higley <greg@prosumma.com>
2023-09-30 02:45:32 -0400

    Formatted Package.swift a bit better

--- Package.swift
+++ Package.swift
@@ -4,43 +4,45 @@
 import PackageDescription
 
 let package = Package(
-    name: "Guise",
-    platforms: [.macOS(.v10_15), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)],
-    products: [
-        .library(
-            name: "Guise",
-            targets: ["Guise"])
-    ],
-    dependencies: [
-      .package(url: "https://github.com/apple/swift-atomics", from: "1.0.0"),
-      .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
-    ],
-    targets: [
-        .target(
-            name: "Guise",
-            dependencies: [
-              .product(name: "Atomics", package: "swift-atomics"),
-              .product(name: "OrderedCollections", package: "swift-collections")
-            ],
-            exclude: [
-              "Lazy/LazyResolver+Async.swift.gyb",
-              "Lazy/LazyResolver+Sync.swift.gyb",
-              "Lazy/LazyTagsResolver+Async.swift.gyb",
-              "Lazy/LazyTagsResolver+Sync.swift.gyb",
-              "Registrar/Auto.swift.gyb",
-              "Registrar/Registrar+Async.swift.gyb",
-              "Registrar/Registrar+Sync.swift.gyb",
-              "Resolver/Resolver+Async.swift.gyb",
-              "Resolver/Resolver+Sync.swift.gyb"
-            ]),
-        .testTarget(
-            name: "GuiseTests",
-            dependencies: ["Guise"],
-            exclude: [
-              "ArgumentTests.swift.gyb",
-              "LazyResolverTests.swift.gyb",
-              "LazyTagsResolverTests.swift.gyb"
-            ]
-        )
-    ]
+  name: "Guise",
+  platforms: [.macOS(.v10_15), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)],
+  products: [
+    .library(
+      name: "Guise",
+      targets: ["Guise"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-atomics", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
+  ],
+  targets: [
+    .target(
+      name: "Guise",
+      dependencies: [
+        .product(name: "Atomics", package: "swift-atomics"),
+        .product(name: "OrderedCollections", package: "swift-collections")
+      ],
+      exclude: [
+        "Lazy/LazyResolver+Async.swift.gyb",
+        "Lazy/LazyResolver+Sync.swift.gyb",
+        "Lazy/LazyTagsResolver+Async.swift.gyb",
+        "Lazy/LazyTagsResolver+Sync.swift.gyb",
+        "Registrar/Auto.swift.gyb",
+        "Registrar/Registrar+Async.swift.gyb",
+        "Registrar/Registrar+Sync.swift.gyb",
+        "Resolver/Resolver+Async.swift.gyb",
+        "Resolver/Resolver+Sync.swift.gyb"
+      ]
+    ),
+    .testTarget(
+      name: "GuiseTests",
+      dependencies: ["Guise"],
+      exclude: [
+        "ArgumentTests.swift.gyb",
+        "LazyResolverTests.swift.gyb",
+        "LazyTagsResolverTests.swift.gyb"
+      ]
+    )
+  ]
 )
